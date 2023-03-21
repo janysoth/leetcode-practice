55d3c2d1a9405bd0abd555e3c50d56409ba1c1d5
Jonny Soth <janysoth@gmail.com>
2023-03-21 00:27:05 -0500

    Refactor codes to simplify the function.

--- mostCommonInt.js
+++ mostCommonInt.js
@@ -1,33 +1,59 @@
 
-function findMostCommonInt(arraySize){
-    const commonCount = {}; // The object to store each element's frequency
-    let mostCount = 0; // The amount of the frequent element shown in the array
-    let mostElement = []; 
-    let randomArray = generateRandomArray(arraySize);
-    //let randomArray =[5,8,4,9,9,8,5,4,10,10];
-    // let randomArray = [];
+// function mostCommonInt(arraySize){
+//     const commonNumber = []; // The array to store each element's frequency
+//     let mostCount = 0; // The amount of the frequent element shown in the array
+//     let mostElement = []; 
+//     let randomArray = generateRandomArray(arraySize);
 
-    // Generate random num 1-10 and push into the array
-    // for (let i = 0; i < arraySize; i++)
-    //     randomArray.push(Math.floor(Math.random()*11));
+//     // Store the frequency of each element in the commonCount object
+//     for (const num of randomArray)
+//     commonNumber[num] = (commonNumber[num] || 0) + 1;
 
-    // Store the frequency of each element in the commonCount object
-    for (const num of randomArray)
-    commonCount[num] = (commonCount[num] || 0) + 1;
-
-    // To Check if there's a tie, show the largest element
-    for (const [key, count] of Object.entries(commonCount)){
-        if (count >= mostCount || (count === mostCount && key > mostElement)){
-        mostCount = count;
-        mostElement = key;
-        }
-    }
+//     // To Check if there's a tie, show the largest element
+//     for (const [key, count] of Object.entries(commonNumber)){
+//         if (count >= mostCount || (count === mostCount && key > mostElement)){
+//         mostCount = count;
+//         mostElement = key;
+//         }
+//     }
     
-    //return Number(mostElement); // To covert an element to a number
+//     //return Number(mostElement); // To covert an element to a number
+//     return console.log(`Here's the array {${randomArray}}.
+//     The number ${mostElement} has shown the most,  ${mostCount} times.`);
+// }
+
+// const generateRandomArray = (arraySize) => {
+//     let randomArray = [];
+//     for (let i = 0; i < arraySize; i++)
+//         randomArray.push(Math.floor(Math.random()*11));
+//     return randomArray;
+// }
+
+// mostCommonInt(4);
+
+function mostCommonInt (arraySize){
+    let maxCount = 0;
+    let randomArray = generateRandomArray(arraySize);
+    let mostCommon = randomArray[0];
+
+    for (let i = 0; i < randomArray.length; i++) {
+        let count = 0;
+        for (let j = 0; j < randomArray.length; j++){
+            if (randomArray[i] === randomArray[j])
+                count++;
+        }
+
+        // If there's tie, it will return the largest integer:
+        if (count > maxCount || (count === maxCount && randomArray[i] > mostCommon)) {
+            maxCount = count;
+            mostCommon = randomArray[i];
+        } 
+    }
     return console.log(`Here's the array {${randomArray}}.
-    The number ${mostElement} has shown the most,  ${mostCount} times.`);
+    The number ${mostCommon} has shown the most,  ${maxCount} times.`); 
 }
 
+// To Generate a random array take in arraySize:
 const generateRandomArray = (arraySize) => {
     let randomArray = [];
     for (let i = 0; i < arraySize; i++)
@@ -35,4 +61,4 @@ const generateRandomArray = (arraySize) => {
     return randomArray;
 }
 
-findMostCommonInt(4);
+mostCommonInt(4);
