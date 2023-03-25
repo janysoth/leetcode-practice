9e692fab9621eb2563cf6aea62fe1997b5b37832
Jonny Soth <janysoth@gmail.com>
2023-03-24 20:48:24 -0500

    Create function to remove duplicates in an array.

---
+++ removeDuplicates.js
@@ -0,0 +1,17 @@
+function removeDuplicates (arraySize){
+    let randomArray = getRandomArray(arraySize);
+    let newArray = [...new Set(randomArray)];
+
+    return console.log(`Here's the random arary: {${randomArray}}.
+    The new array is {${newArray}}`); 
+}
+
+// To Generate a random array giving the arraySize:
+const getRandomArray = (arraySize) => {
+    let myArray = [];
+    for (let i = 0; i < arraySize; i++)
+        myArray.push(Math.floor(Math.random()*11));
+    return myArray;
+}
+
+removeDuplicates(10);
