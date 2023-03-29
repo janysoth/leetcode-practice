6a60f2d28f469f77b419afb4ded2e1a30836d160
Jonny Soth <janysoth@gmail.com>
2023-03-28 20:37:43 -0500

    Add functions into the class.

--- class.js
+++ class.js
@@ -2,8 +2,19 @@ class Movie {
     constructor(title, year) {
         this.title = title;
         this.year = year;
+    } 
+    
+    Year(){
+        return this.year;
     }
+
+    Titlte(){
+        return this.title;
+    }
+
 }
 
 const myMovie = new Movie('Cocaine Bear', 2023);
-console.log(myMovie);
+
+let message = `"${myMovie.Titlte()}" came out in the year, ${myMovie.Year()}.`;
+console.log(message);
