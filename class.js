ace64302468f90861f85efaf7fa4c7b6d676a8cb
Jonny Soth <janysoth@gmail.com>
2023-03-28 20:29:28 -0500

    Learn how to declare class in JS.

---
+++ class.js
@@ -0,0 +1,9 @@
+class Movie {
+    constructor(title, year) {
+        this.title = title;
+        this.year = year;
+    }
+}
+
+const myMovie = new Movie('Cocaine Bear', 2023);
+console.log(myMovie);
